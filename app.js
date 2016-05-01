f0173a5149b4a83e1895f4481feb79ea54d84ebc
JamesWClark <jwclark@rockhursths.edu>
2016-05-01 14:28:28 -0500

    v1

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-var http = require("http");
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
+var http = require('http');
+var fs = require('fs');
+var data = fs.readFileSync('./site/index.html', 'UTF8', function(err) {
+    if(err) throw err;
+});
+
+var server = http.createServer(function(req,res) {
+    res.writeHead(200, { 'Content-Type' : 'text/html'});
+    res.end(data);
 }).listen(process.env.PORT || 8888);
