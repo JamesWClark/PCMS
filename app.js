9daebe247970f70d7c6deb55f401383ee4fc1e6a
JamesWClark <jwclark@rockhursths.edu>
2016-05-01 20:53:59 -0500

    works on aws - cool!

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-
-var http = require('http');
+var express = require('express');
+var app = express();
 var fs = require('fs');
-var data = fs.readFileSync('./site/index.html', 'UTF8', function(err) {
-    if(err) throw err;
+
+app.use('/static', express.static(__dirname + '/static'));
+app.use(express.static(__dirname + 'site'));
+
+app.get('/', function(req,res) {
+    fs.readFile('./site/index.html', function(err, data) {
+        res.writeHead(200, { 'Content-Type' : 'text/html' });
+        res.end(data, 'utf-8');
+    });
 });
 
-var server = http.createServer(function(req,res) {
-    res.writeHead(200, { 'Content-Type' : 'text/html'});
-    res.end(data);
-}).listen(process.env.PORT || 8888);
+app.listen(process.env.PORT || 1898, function() {
+    console.log('listening on 1898');
+});
